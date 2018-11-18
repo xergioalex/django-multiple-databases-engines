93e8fe16e16eb9ac597a8d277cea4cdba250ba1a
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2018-11-18 16:41:59 -0500

    Docker configs

--- docker/production/django/uwsgi.sh
+++ docker/production/django/uwsgi.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -o errexit
 set -o pipefail
 set -o nounset
 
-python /app/manage.py collectstatic --noinput
+#python /app/manage.py collectstatic --noinput
 python /app/manage.py migrate --noinput
 /usr/local/bin/uwsgi --ini /app/uwsgi.ini --logto /app/uwsgi.log
