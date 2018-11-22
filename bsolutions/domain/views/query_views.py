d91cad4e9bcd6e29a7698a7b7a1161fa077628f1
nelson.holic <nelson.holic@gmail.com>
2018-11-22 11:13:14 -0500

    testing action on viewset

--- bsolutions/domain/views/query_views.py
+++ bsolutions/domain/views/query_views.py
@@ -1,7 +1,9 @@
+from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 
 from rest_framework.response import Response
 
+from bsolutions.domain.models.profile import Cliente
 from bsolutions.domain.models.purchase import Compra
 
 
@@ -12,3 +14,10 @@ class AllCompras(ViewSet):
         limit = request.GET.get('limit', 100)
         list(Compra.objects.using(db).all()[:int(limit)])
         return Response({'db': db})
+
+    @action(methods=['GET'])
+    def get_ventas_de_un_usuario(self, request):
+        db = request.GET.get('db', 'default')
+        limit = request.GET.get('limit', 100)
+        list(Cliente.objects.using(db).all()[:int(limit)].prefetch_related('compra_set__compraproducto_set'))
+        return Response({})
